d5f98840c4ed0a8dda6e894d6edfdddf7ee3e641
drmacm <drmac.mladen@gmail.com>
2020-08-15 20:30:29 +0200

    Adding a spinner

---
+++ PDFiller.Website/Services/SpinnerUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace PDFiller.Website.Services
+{
+    public static class SpinnerUtil
+    {
+        public static ValueTask ShowSpinner(this IJSRuntime js)
+        {
+            return js.InvokeVoidAsync("showSpinner");
+        }
+        public static ValueTask HideSpinner(this IJSRuntime js)
+        {
+            return js.InvokeVoidAsync("hideSpinner");
+        }
+    }
+}
